9433738d04002498372c1ec4e76568818b497656
Tony <gth1123@naver.com>
2022-05-24 22:26:32 +0900

    zustand 보충

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import useNameStore from '@/store/useNameStore';
 import { useCountStore } from './StudyZustand';
 
 function About() {
-  const { name } = useNameStore();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { name, setName } = useNameStore();
   const { count } = useCountStore();
   return (
     <>
       <h1>about</h1>
       <div>count : {count}</div>
       <div>name : {name}</div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setName(inputRef.current!.value);
+      }}
+      >
+        <input ref={inputRef} type="text" placeholder="please input your name" />
+      </form>
       <Link to="/">go home</Link>
       <Link to="/study/zustand">/study/zustand</Link>
     </>
