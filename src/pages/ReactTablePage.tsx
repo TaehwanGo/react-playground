4a62a4bbc929327e6828580a61ab92e445151c05
TaehwanGo <gth1123@builton.co.kr>
2022-06-09 15:29:21 +0900

    Table : data에 useMemo 적용

--- src/pages/ReactTablePage.tsx
+++ src/pages/ReactTablePage.tsx
@@ -1,5 +1,5 @@
 // react-table 공부
-import React from "react";
+import React, { useMemo } from "react";
 import { faker } from '@faker-js/faker';
 import ReactTable from "@/components/ReactTable";
 
@@ -8,13 +8,15 @@ faker.seed(100);
 function ReactTablePage() {
   const columns = ["Name", "Email", "Phone"];
 
-  const data = Array(53)
-    .fill('')
-    .map(() => ({
-      name: faker.name.lastName() + faker.name.firstName(),
-      email: faker.internet.email(),
-      phone: faker.phone.phoneNumber(),
-    }));
+  const data = useMemo(() => {
+    return Array(53)
+      .fill('')
+      .map(() => ({
+        name: faker.name.lastName() + faker.name.firstName(),
+        email: faker.internet.email(),
+        phone: faker.phone.phoneNumber(),
+      }));
+  }, []);
 
   return (
     <ReactTable columns={columns} data={data} />
