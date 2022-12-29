b2436fd27c7825f9c8326981b7f050ac1521f597
TaehwanGo <gth1123@bubblecon.io>
2022-12-29 18:13:11 +0900

    edit component by editor

--- src/pages/ToastUiEditor.tsx
+++ src/pages/ToastUiEditor.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Editor } from '@toast-ui/react-editor';
 import styled from 'styled-components';
-import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import CommonLayout from '@/components/layouts/CommonLayout';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import 'tui-color-picker/dist/tui-color-picker.css';
-import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import EditCard from '@/components/molecules/EditCard';
 
 const CustomCommonLayout = styled(CommonLayout)`
   background-color: #fff;
@@ -23,16 +19,31 @@ const CustomCommonLayout = styled(CommonLayout)`
 `;
 
 const ToastUiEditor = () => {
+  const [focusedComponentId, setFocusedComponentId] = React.useState<string>('');
+  const handleClickComponent = (id: string) => {
+    setFocusedComponentId(id);
+  };
+
+  // const handleClickBackground = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+
   return (
-    <CustomCommonLayout>
-      <Editor
-        initialValue="hello react editor world!"
-        previewStyle="vertical"
-        height="600px"
-        initialEditType="wysiwyg"
-        useCommandShortcut
-        plugins={[colorSyntax]}
-        toolbarItems={[['heading', 'bold']]}
+    // CustomCommonLayout click event가 정확히 일치하는 경우에만 발생한다
+    <CustomCommonLayout
+      onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (e.target === e.currentTarget) {
+          setFocusedComponentId('');
+        }
+      }}
+    >
+      <EditCard
+        id="card-1"
+        isEditorMode={focusedComponentId === 'card-1'}
+        handleClickComponent={handleClickComponent}
+      />
+      <EditCard
+        id="card-2"
+        isEditorMode={focusedComponentId === 'card-2'}
+        handleClickComponent={handleClickComponent}
       />
     </CustomCommonLayout>
   );
