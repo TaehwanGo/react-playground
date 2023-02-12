304e837b19d046e4a57b2b0568569e2cd1bdf5d9
Tony <gth1123@naver.com>
2023-02-12 20:03:50 +0900

    circular progress

--- src/pages/CircularProgressbarPage.tsx
+++ src/pages/CircularProgressbarPage.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 200px;
+  height: 200px;
+  padding: 20px;
+`;
 
 /**
  * https://www.npmjs.com/package/react-circular-progressbar
@@ -8,12 +15,33 @@ import 'react-circular-progressbar/dist/styles.css';
 const CircularProgressbarPage = () => {
   const percentage = 66;
 
+  const [value, setValue] = React.useState(0);
+
   return (
     <div>
       <h1>Circular Progressbar</h1>
-      <div style={{ width: 200, height: 200 }}>
+      <Container>
         <CircularProgressbar value={percentage} text={`${percentage}%`} />
-      </div>
+      </Container>
+      <Container>
+        <CircularProgressbar
+          value={value}
+          text={`${value}%`}
+          circleRatio={0.75}
+          styles={buildStyles({
+            rotation: 1 / 2 + 1 / 8,
+            strokeLinecap: 'butt',
+            trailColor: '#eee',
+          })}
+        />
+        <input
+          type="text"
+          onChange={(e) => {
+            setValue(parseInt(e.target.value === '' ? '0' : e.target.value, 10));
+          }}
+          style={{ width: '100%', padding: '10px', boxSizing: 'border-box' }}
+        />
+      </Container>
     </div>
   );
 };
